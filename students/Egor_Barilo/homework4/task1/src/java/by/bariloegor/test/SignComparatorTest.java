be506b5c7849c626aa7d0ecc8608bc36457bea66
barilo-egor <barilo.egor@gmail.com>
2020-04-09 23:57:24 +0300

    sign comparator

---
+++ students/Egor_Barilo/homework4/task1/src/java/by/bariloegor/test/SignComparatorTest.java
@@ -0,0 +1,41 @@
+package by.bariloegor.test;
+
+import by.bariloegor.service.SignComparatorService;
+
+public class SignComparatorTest {
+    private SignComparatorService signComparatorService = new SignComparatorService();
+
+    private void assertEquals(String testName, String expected, String actual){
+        boolean result = expected.equals(actual);
+        if(result){
+            System.out.println("\n[INFO] " + testName + " test passed successfully.");
+        } else {
+            System.out.println("\n[ERROR] " + testName + " test is failed.");
+        }
+    }
+
+    private void comparePositiveNumberTest(){
+        String expected = "Number is positive.";
+        String actual = signComparatorService.compare(1);
+        assertEquals("Positive number", expected, actual);
+    }
+
+    private void compareNegativeNumberTest(){
+        String expected = "Number is negative.";
+        String actual = signComparatorService.compare(-1);
+        assertEquals("Negative number", expected, actual);
+    }
+
+    private void compareZeroTest(){
+        String expected = "Number is equal to zero.";
+        String actual = signComparatorService.compare(0);
+        assertEquals("Zero number", expected, actual);
+    }
+
+    public void signComparatorTest(){
+        comparePositiveNumberTest();
+        compareNegativeNumberTest();
+        compareZeroTest();
+        System.out.println();
+    }
+}
