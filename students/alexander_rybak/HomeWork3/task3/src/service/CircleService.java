cdbfbed5fa2302dd89ef9120e98c5f49335aa8e1
Alexander Rybak <61635826+Alexander-1313@users.noreply.github.com>
2020-04-10 19:23:17 +0300

    Delete CircleService.java

--- students/alexander_rybak/HomeWork3/task3/src/service/CircleService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import bean.Circle;
-
-public class CircleService {
-    public double calculateArea(Circle circle) {
-        return Math.PI * Math.pow(circle.getRadius(), 2);
-    }
-}
