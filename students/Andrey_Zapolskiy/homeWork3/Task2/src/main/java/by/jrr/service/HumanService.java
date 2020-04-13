f2b126e2e7b108b64bfb0faf82ae8fd6493f89fc
andrey zapolskiy <58439394+asraysun@users.noreply.github.com>
2020-04-13 18:21:26 +0300

    Update HumanService.java

--- students/Andrey_Zapolskiy/homeWork3/Task2/src/main/java/by/jrr/service/HumanService.java
+++ students/Andrey_Zapolskiy/homeWork3/Task2/src/main/java/by/jrr/service/HumanService.java
@@ -10,15 +10,7 @@ public class HumanService {
     public void saveHuman(Human human){
             HumanRepository.humanList.add(human);
         }
-//        public Human findHumanByName(String humanName){
-//            for(Human oneHuman: HumanRepository.humanList) {
-//                if (oneHuman.getName().equals(humanName)) {
-//                    return oneHuman;
-////                return new Human("Charlie", 35);
-//                }
-//            }
-//            return new Human();
-//        }
+
         public Human greet(Human human) {
 
             System.out.println("Hi! My name is " + human.getName()+ ", I'm "
