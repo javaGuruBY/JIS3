aa41034c7a4e09390082648481fae645e3e4eae8
Максим Клещёв <mmkleschev@gmail.com>
2020-04-14 11:21:13 +0300

    trying maven with junit

---
+++ students/Maxim_Kliashchou/HomeWork04/Task01_maven/SignComparator/src/test/java/by/maxim/kliashchou/service/SignComparatorServiceTest.java
@@ -0,0 +1,11 @@
+package by.maxim.kliashchou.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SignComparatorServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void compare() {
+        assertEquals(1, 1, "checkTest");
+    }
+}
