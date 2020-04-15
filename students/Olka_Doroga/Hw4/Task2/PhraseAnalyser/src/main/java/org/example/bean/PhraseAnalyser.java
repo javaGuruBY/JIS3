039be1384ac60505723f69906d2b035066862e64
olka_doroga <62174076+olkadoroga@users.noreply.github.com>
2020-04-15 23:01:22 +0300

    Update PhraseAnalyser.java

--- students/Olka_Doroga/Hw4/Task2/PhraseAnalyser/src/main/java/org/example/bean/PhraseAnalyser.java
+++ students/Olka_Doroga/Hw4/Task2/PhraseAnalyser/src/main/java/org/example/bean/PhraseAnalyser.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class PhraseAnalyser implements Serializable {
-    String text;
+    private String text;
 
     public PhraseAnalyser() {
     }
