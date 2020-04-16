6af5d6b488c7ab7d4616c3150152262e35fd8a8f
Tane4kka <Tirano4ka@gmail.com>
2020-04-17 01:18:34 +0300

    I did my HW3 T2

---
+++ students/Tanya_Sorokina/HW3/Task1/Class_Human/src/main/java/by/jrr/bean/Human.java
@@ -0,0 +1,49 @@
+package by.jrr.bean;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Human implements Serializable {
+    private String name;
+    private int age;
+
+
+    public Human(){
+    }
+
+    public Human(String name, int age) {
+    this.name = name;
+    this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+    Human human = (Human) obj;
+    return age == human.age &&
+            Objects.equals(name, human.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+}
