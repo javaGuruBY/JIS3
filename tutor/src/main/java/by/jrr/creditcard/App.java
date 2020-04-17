fc705d8d8a39ea6513350869b418df2b08a02eee
max-java <6666350@gmail.com>
2020-04-17 21:44:15 +0300

    Initial commit of a project template

---
+++ tutor/src/main/java/by/jrr/creditcard/App.java
@@ -0,0 +1,14 @@
+package by.jrr.creditcard;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+
+public class App {
+    public static void main(String[] args) {
+        Logger log = LoggerFactory.getLogger(App.class);
+
+        log.info("\"Hello\" = " + "Hello");
+    }
+}
---
+++ tutor/src/test/java/by/jrr/creditcard/AppTest.java
@@ -0,0 +1,13 @@
+package by.jrr.creditcard;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void main() {
+        assertEquals(true, true);
+    }
+}
