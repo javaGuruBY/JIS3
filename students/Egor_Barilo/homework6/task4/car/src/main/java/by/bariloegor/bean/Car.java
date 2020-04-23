40f3e39733b11c01a22eaea81287ba27bb455765
barilla <barilo.egor@gmail.com>
2020-04-24 00:08:03 +0300

    carservice

---
+++ students/Egor_Barilo/homework6/task4/car/src/main/java/by/bariloegor/bean/Car.java
@@ -0,0 +1,79 @@
+package by.bariloegor.bean;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Car  implements Serializable{
+    private String model;
+    private String color;
+    private int maxSpeed;
+    private int actualSpeed;
+
+    public Car() {
+    }
+
+    public Car(String model, String color, int maxSpeed) {
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.actualSpeed = 0;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getActualSpeed() {
+        return actualSpeed;
+    }
+
+    public void setActualSpeed(int actualSpeed) {
+        this.actualSpeed = actualSpeed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return maxSpeed == car.maxSpeed &&
+                actualSpeed == car.actualSpeed &&
+                Objects.equals(model, car.model) &&
+                Objects.equals(color, car.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, color, maxSpeed, actualSpeed);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", actualSpeed=" + actualSpeed +
+                '}';
+    }
+}
