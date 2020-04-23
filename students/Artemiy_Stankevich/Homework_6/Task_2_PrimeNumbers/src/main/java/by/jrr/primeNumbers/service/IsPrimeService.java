ea38afb5913adcc4df684b776a8058592ff266de
Your Name <articzander@mail.ru>
2020-04-23 21:48:56 +0300

    Prime Numbers hw 6 task 2

---
+++ students/Artemiy_Stankevich/Homework_6/Task_2_PrimeNumbers/src/main/java/by/jrr/primeNumbers/service/IsPrimeService.java
@@ -0,0 +1,12 @@
+package by.jrr.primeNumbers.service;
+
+public class IsPrimeService {
+    public boolean isPrime(int n) {
+        if (n % 2== 0) return false;
+        for(int i = 3; i * i <= n; i += 2) {
+            if(n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
