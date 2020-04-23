6a4250cd49c08ca94f20d4d206e740efc1b890fc
abbas29n <abbas.n@outlook.com>
2020-04-23 12:44:49 +0300

    H6T1

---
+++ students/Abbas_Nasser/HW6/primeNumbers/src/main/java/by/abs/service/PrimeNumbersService.java
@@ -0,0 +1,42 @@
+package by.abs.service;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PrimeNumbersService {
+
+    public List<Integer> sumOfPrimeNumbers(){
+
+        int left = 1;
+        int right = 500;
+        int sum = 0;
+        int count = 0;
+
+        for (int i = left; i <= right ; i++){
+            if (i ==17 || i == 71) {
+                continue;
+            }
+            if (isPrimeNumber(i)){
+                sum =+ i;
+                count++;
+            }
+            if (count ==50){
+                break;
+            }
+        }
+                return Arrays.asList(sum, count);
+    }
+
+
+    private boolean isPrimeNumber(int number) {
+        if (number ==1)
+            return false;
+        for (int i = 2;i <= Math.sqrt(number); i++){
+            if (number %i ==0) {
+                return false;
+            }
+        }
+return true;
+    }
+}
