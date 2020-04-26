705c6f0e7e22a4df16603ced6e258c9ec16ddbcc
abbas29n <abbas.n@outlook.com>
2020-04-26 16:25:33 +0300

    H7T2

---
+++ students/Abbas_Nasser/HW7/task2/src/test/java/by/abs/service/CopyServiceTest.java
@@ -0,0 +1,36 @@
+package by.abs.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CopyServiceTest {
+    private CopyService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new CopyService();
+    }
+
+    @Test
+    void copyInRange() {
+        String testMessage = "Should return new array with elements in range";
+        int[] leftBound = {8,9};
+        int[] rightBound = {9,10};
+        int[][] array = {
+                { 7, 8, 9, 10, 11},
+                {1, 2, 3, 4, 5, 6} };
+        int[][] expected = {
+                {8, 9},
+                {} };
+
+        for (int i = 0; i < expected.length; i++){
+            int[] actual = service.copyInRange(array[i], leftBound[i], rightBound[i]);
+            assertEquals(Arrays.toString(expected[i]), Arrays.toString(actual), testMessage);
+        }
+
+    }
+}
