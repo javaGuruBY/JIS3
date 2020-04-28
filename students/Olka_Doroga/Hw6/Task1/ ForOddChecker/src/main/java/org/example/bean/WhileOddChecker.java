b2d82fa1f8529b7f7fac943f72a4c6d2e6da6639
olkadoroga <doroga95@mail.ru>
2020-04-28 21:08:01 +0300

    Olka_Doroga Hw6 Task1

---
+++ students/Olka_Doroga/Hw6/Task1/ ForOddChecker/src/main/java/org/example/bean/WhileOddChecker.java
@@ -0,0 +1,14 @@
+package org.example.bean;
+
+public class WhileOddChecker {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 50) {
+            if (i % 2 != 0) {
+                System.out.println("i = " + i + "; ");
+            }
+            i++;
+        }
+    }
+}
+
