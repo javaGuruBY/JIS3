55a00e17b15d566f097a443bdc211ca820376d78
Vitaliy Khilko <62650726+Vitaliy-Hilko@users.noreply.github.com>
2020-04-28 11:29:12 +0200

    Delete DogRepository.java

--- students/VitaliyKhilko/HomeWork3/Task1/Dog/src/main/java/by/khilko/repository/DogRepository.java
+++
@@ -1,8 +0,0 @@
-package by.khilko.repository;
-import by.khilko.bean.Dog;
-import java.util.ArrayList;
-import java.util.List;
-
-public class DogRepository {
-    public static List<Dog> dogList = new ArrayList<>();
-}
