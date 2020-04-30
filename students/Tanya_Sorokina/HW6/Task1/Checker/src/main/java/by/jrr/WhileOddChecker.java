a23d12235d7b52abde92c7fc5fe238c32982f296
Tane4kka <Tirano4ka@gmail.com>
2020-04-30 05:43:06 +0300

    I did my HW6 T1

---
+++ students/Tanya_Sorokina/HW6/Task1/Checker/src/main/java/by/jrr/WhileOddChecker.java
@@ -0,0 +1,11 @@
+package by.jrr;
+
+public class WhileOddChecker {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 50) {
+            System.out.println("i = " + i);
+            i = i+2;
+        }
+    }
+}
