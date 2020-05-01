b40a508556dc6f5128ef328d45120620e9cf942b
abbas29n <abbas.n@outlook.com>
2020-05-01 15:55:08 +0300

    H9T3

---
+++ students/Abbas_Nasser/HW9/task3/src/main/java/by/abs/bean/Library.java
@@ -0,0 +1,13 @@
+package by.abs.bean;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Library {
+    public static final Map<String, List<String>>library = new HashMap<>();
+
+
+    public Library() {
+    }
+}
