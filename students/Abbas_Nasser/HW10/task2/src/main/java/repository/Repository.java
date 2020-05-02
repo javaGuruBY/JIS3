611bbd6ab52d73cc037989ddd709cfcd4be4eff6
abbas29n <abbas.n@outlook.com>
2020-05-02 18:11:38 +0300

    H10T2

---
+++ students/Abbas_Nasser/HW10/task2/src/main/java/repository/Repository.java
@@ -0,0 +1,14 @@
+package repository;
+
+import exceptions.ItemNotFoundException;
+
+import java.util.List;
+
+public interface Repository <T> {
+
+    List<T> findAll();
+    T findById(String id) throws ItemNotFoundException;
+    void save(T item);
+    void delete(String id) throws ItemNotFoundException;
+
+}
