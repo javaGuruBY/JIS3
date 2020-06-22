18d5933723d6c14ee42a4326c593ab165a0c7484
max <maxim.kliashchou@gmail.com>
2020-06-22 19:05:28 +0300

    add new validation rules

--- students/Maxim_Kliashchou/Java2/initial-project/src/com/javaguru/shoppinglist/service/validation/ProductDiscountValidationRule.java
+++ students/Maxim_Kliashchou/Java2/initial-project/src/com/javaguru/shoppinglist/service/validation/ProductDiscountValidationRule.java
@@ -11,5 +11,14 @@ public class ProductDiscountValidationRule implements ProductValidationRule{
         if (product.getDiscount().compareTo(new BigDecimal(100)) > 0){
             throw new IllegalArgumentException("Discount can't be more than 100");
         }
+
+        if (product.getDiscount().compareTo(BigDecimal.ZERO) < 0){
+            throw new IllegalArgumentException("Discount can't be less than 0");
+        }
+
+        if (product.getPrice().compareTo(new BigDecimal(20)) < 0
+                && product.getDiscount().compareTo(BigDecimal.ZERO) != 0){
+            throw new IllegalArgumentException("Discount can't be applied to products with price less than 20$");
+        }
     }
 }
