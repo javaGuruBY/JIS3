2c7d9f407950e82b799e6cc30861ab1871daccc8
Tane4kka <Tirano4ka@gmail.com>
2020-07-03 17:06:23 +0300

    I did my HW9T2

---
+++ students/Tanya_Sorokina/HW9/T2/Unique_Word_Counter/src/main/java/by/jrr/bean/UniqueWordCounter.java
@@ -0,0 +1,11 @@
+package by.jrr.bean;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class UniqueWordCounter {
+
+    public static Map<String, Integer> counter = new HashMap<String, Integer>();
+
+}
