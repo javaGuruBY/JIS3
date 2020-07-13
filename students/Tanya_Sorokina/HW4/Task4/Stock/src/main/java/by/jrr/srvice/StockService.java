feb0b9bc5a2da8a8c8c9480093b5fcf03d6bf476
Tanya Sorokina <62208319+Tane4kka@users.noreply.github.com>
2020-07-13 19:44:32 +0300

    Update StockService.java
    fix code

--- students/Tanya_Sorokina/HW4/Task4/Stock/src/main/java/by/jrr/srvice/StockService.java
+++ students/Tanya_Sorokina/HW4/Task4/Stock/src/main/java/by/jrr/srvice/StockService.java
@@ -4,22 +4,15 @@ import by.jrr.bean.Stock;
 public class StockService {
 
     public void updatePrice(Stock stock, double correctPrice) {
-//        Stock stock = new Stock();
-//        stock.setCurrentValue(correctPrice);
         if (correctPrice < stock.getCurrentValue()) {
             stock.setMinimumPrice(correctPrice);
         } else if (correctPrice > stock.getCurrentValue()) {
             stock.setMaximumPrice(correctPrice);
         }
-
-//        stock.setMinimumPrice(Math.min(stock.getCurrentValue(), stock.getMinimumPrice()));
-//        stock.setMaximumPrice(Math.max(stock.getCurrentValue(), stock.getMaximumPrice()));
     }
 
     public void printInformation(Stock stock){
             System.out.println("Company = " + stock.getCompanyName() + ", Current Price = " + stock.getCurrentValue()
                     + ", Min price = " + stock.getMinimumPrice() + ", Max price = " + stock.getMaximumPrice());
         }
-
-
 }
